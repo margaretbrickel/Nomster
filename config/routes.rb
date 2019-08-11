d88e2e7303a3fd42445194fd1f2ddabec3fd7a65
Margaret Brickel <mbrickel@gmail.com>
2019-08-11 23:55:26 +0000

    Building the user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   root 'places#index'
   resources :places do
     resources :comments, only: :create
-    resources :photos
+    resources :photos, only: :create
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
+  resources :users, only: :show
 end
